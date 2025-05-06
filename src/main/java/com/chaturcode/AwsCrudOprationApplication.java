e97b29ca1582f7e23839f9dc894f28befea8e66e
dip8 <dipaklokhande81@gmail.com>
2025-05-06 20:17:42 +0530

    initial Commit

---
+++ src/main/java/com/chaturcode/AwsCrudOprationApplication.java
@@ -0,0 +1,13 @@
+package com.chaturcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCrudOprationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCrudOprationApplication.class, args);
+	}
+
+}
