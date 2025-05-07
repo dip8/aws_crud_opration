b928768b3bdf82f2df8ff8c4454cda6da3dd5778
Dipak.Lokhande <dipak.lokhande@medimaze.in>
2025-05-07 13:20:09 +0530

    Modification according deployment

--- src/main/java/com/chaturcode/controller/EmployeeController.java
+++ src/main/java/com/chaturcode/controller/EmployeeController.java
@@ -1,4 +1,67 @@
 package com.chaturcode.controller;
 
+import com.chaturcode.entity.Employee;
+import com.chaturcode.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/employees")
+@Slf4j
+
 public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Employee> signup(@RequestBody Employee employee){
+        log.info("@@@Try to sign up for the Employee"+ employee.getEmpName());
+        return ResponseEntity.ok(employeeService.signup(employee));
+    }
+
+    @GetMapping("/signin/{empEmailId}/{empPassword}")
+    public ResponseEntity<Boolean> signIn(@PathVariable String empEmailId, @PathVariable String empPassword){
+        return ResponseEntity.ok(employeeService.signIn(empEmailId, empPassword));
+    }
+
+    @GetMapping("/findbyid/{empId}")
+    public ResponseEntity<Optional<Employee>> findById(@PathVariable int empId){
+        return ResponseEntity.ok(employeeService.findByID(empId));
+    }
+
+    @GetMapping("/findbyname")
+    public ResponseEntity<List<Employee>> findByName(
+            @RequestParam(defaultValue = "CHATURCODE", required = false) String empName) {
+
+        List<Employee> filtered = employeeService.findAll().stream()
+                .filter(emp -> emp.getEmpName().equals(empName))
+                .toList();
+
+        return ResponseEntity.ok(filtered);
+    }
+
+    @PutMapping("/update/{empId}")
+    public ResponseEntity<Employee> update(@PathVariable int empId, @RequestBody Employee employee){
+        Employee employee1 = employeeService.findByID(empId).get();
+        employee1.setEmpName(employee.getEmpName());
+        employee1.setEmpAddress(employee.getEmpAddress());
+        employee1.setEmpSalary(employee.getEmpSalary());
+        employee1.setEmpContactNumber(employee.getEmpContactNumber());
+        employee1.setEmpDOB(employee.getEmpDOB());
+        employee1.setEmpEmailId(employee.getEmpEmailId());
+        employee1.setEmpPassword(employee.getEmpPassword());
+
+        return ResponseEntity.ok(employeeService.update(employee1));
+    }
+
+    @DeleteMapping("/deletebyid/{empId}")
+    public ResponseEntity<String> deleteById(@PathVariable int empId){
+        employeeService.deleteById(empId);
+        return ResponseEntity.ok("Data Deleted by Successfully");
+    }
 }
--- src/main/java/com/chaturcode/service/EmployeeService.java
+++ src/main/java/com/chaturcode/service/EmployeeService.java
@@ -1,9 +1,13 @@
 package com.chaturcode.service;
 
 import com.chaturcode.entity.Employee;
+import com.chaturcode.exception.RecordNotFoundException;
 import com.chaturcode.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -11,6 +15,35 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    public Employee signup(Employee employee){
+        return employeeRepository.save(employee);
+    }
 
+    public boolean signIn(String empEmailId, String empPassword){
+            boolean flag = false;
+
+            Employee employee = employeeRepository.findByEmpEmailIdAndEmpPassword(empEmailId, empPassword);
+
+            if(employee!=null){
+                flag=true;
+            }
+            return flag;
+    }
+
+    public Optional<Employee> findByID(int empId){
+        return Optional.ofNullable(employeeRepository.findById(empId).orElseThrow(()-> new RecordNotFoundException("Employee Id does not exists")));
+    }
+
+    public List<Employee> findAll(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee update(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteById(int empId){
+        employeeRepository.deleteById(empId);
+    }
 }
 
